47379246f0e12bddb64fa1b74fab05294fcf293e
adozaraz <minecraft-no.adozaraz@yandex.ru>
2022-09-19 14:32:05 +0400

    Init commit

---
+++ Planet.py
@@ -0,0 +1,64 @@
+import Utils
+
+
+class Planet:
+    def __init__(self, **kwargs):
+        self.__mass = kwargs['mass']
+        self.__point = kwargs['point']
+        self.__velocity = kwargs['velocity']
+        self.__acceleration = kwargs['acceleration']
+
+    @property
+    def mass(self):
+        return self.__mass
+
+    @property
+    def point(self):
+        return self.__point
+
+    @property
+    def velocity(self):
+        return self.__velocity
+
+    @property
+    def acceleration(self):
+        return self.__acceleration
+
+    @mass.setter
+    def mass(self, mass) -> None:
+        self.__mass = mass
+
+    @point.setter
+    def point(self, point) -> None:
+        self.__point = point
+
+    @velocity.setter
+    def velocity(self, velocity) -> None:
+        self.__velocity = velocity
+
+    @acceleration.setter
+    def acceleration(self, acceleration) -> None:
+        self.__acceleration = acceleration
+
+    def __eq__(self, other):
+        if not isinstance(other, Planet):
+            return NotImplemented
+
+        return self.mass == other.mass and self.point == other.point \
+               and self.velocity == other.velocity and self.acceleration == other.acceleration
+
+    def calculateStep(self, time: float, dt: float, planets, scheme: int = 0):
+        """
+        :param dt: Шаг по времени
+        :param planets: Все планеты
+        :param time: Текущее время шага
+        :param scheme: По какому методу будет вычисляться
+        :return: None
+        """
+        if scheme == Utils.SCHEMES.EULER:
+            acceleration = Utils.Acceleration(0, 0, 0)
+            for planet in planets:
+                if self == planet:
+                    continue
+                acceleration += Utils.Acceleration.calculateAcceleration(self.point, planet.point)
+            self.velocity.x += acceleration.x * dt
---
+++ Utils.py
@@ -0,0 +1,78 @@
+import math
+from enum import Enum
+from scipy.constants import G
+
+
+class SCHEMES(Enum):
+    EULER = 0
+    VERLET = 1
+    BIMAN = 2
+
+
+class Characteristic:
+    def __init__(self, x: float = 0, y: float = 0, z: float = 0):
+        self.__x: float = x
+        self.__y: float = y
+        self.__z: float = z
+
+    def __iadd__(self, other):
+        self.__x += other.x
+        self.__y += other.y
+        self.__z += other.z
+        return self
+
+    def __eq__(self, other):
+        if not isinstance(other, Characteristic):
+            return NotImplemented
+
+        return type(self) == type(other) and self.x == other.x and self.y == other.y and self.z == other.z
+
+    @property
+    def x(self) -> float:
+        return self.__x
+
+    @property
+    def y(self) -> float:
+        return self.__y
+
+    @property
+    def z(self) -> float:
+        return self.__z
+
+    @x.setter
+    def x(self, x: float) -> None:
+        self.__x = x
+
+    @y.setter
+    def y(self, y: float) -> None:
+        self.__y = y
+
+    @z.setter
+    def z(self, z: float) -> None:
+        self.__z = z
+
+
+class Point(Characteristic):
+    @staticmethod
+    def calculateScalarDistance(firstPoint, secondPoint):
+        return math.sqrt((firstPoint.x - secondPoint.x) ** 2 +
+                         (firstPoint.y - secondPoint.y) ** 2 + (firstPoint.z - secondPoint.z) ** 2)
+
+
+class Velocity(Characteristic):
+    pass
+
+
+class Acceleration(Characteristic):
+
+    def recalculateAcceleration(self, acceleration, radius, point):
+        self.x += acceleration * (self.x - point.x) / radius
+        self.y += acceleration * (self.y - point.y) / radius
+        self.z += acceleration * (self.z - point.z) / radius
+
+    @staticmethod
+    def calculateAcceleration(planet, otherPlanet):
+        r = Point.calculateScalarDistance(planet.point, otherPlanet.point)
+        a = planet.mass * G / (r ** 2)
+        return Acceleration(x=a * (planet.x - otherPlanet.x) / r, y=a * (planet.y - otherPlanet.y) / r,
+                            z=a * (planet.z - otherPlanet.z) / r)
